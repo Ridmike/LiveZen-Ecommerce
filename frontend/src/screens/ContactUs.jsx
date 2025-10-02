8bceec042c4b0b5a0939543c5fc88305d1f1ea19
Shakil Ridmike <ridmikew@gmail.com>
2025-10-02 16:38:52 +0530

    Add Color palette and edit Order Route
    Add Color palette and edit Order Route

--- frontend/src/screens/ContactUs.jsx
+++ frontend/src/screens/ContactUs.jsx
@@ -212,7 +212,7 @@ export default function ContactUs() {
             <div className="mt-8 flex justify-end">
               <button
                 type="submit"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 Send message
               </button>
